7941682119ecc096ec2a02b4cf28e4eb7d031c63
SON NGUYEN <myantj3m@gmail.com>
2018-12-20 14:30:04 +0700

    error

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,9 @@
 package hello;
 
+import hello.dao.FileUploadDAO;
+import hello.dao.FileUploadDAOImpl;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 import hello.storage.StorageProperties;
 import hello.storage.StorageService;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -15,6 +20,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    @Bean(name = "fileUploadDAO")
+    public FileUploadDAO getUserDao(SessionFactory sessionFactory) {
+        return new FileUploadDAOImpl(sessionFactory);
     }
 
     @Bean
